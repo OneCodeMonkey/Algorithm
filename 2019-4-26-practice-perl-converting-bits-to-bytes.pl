6d8dc155a774f64406f8bf5e3ffd7eb78cb9f487
OneCodeMonkey <1460018362@qq.com>
2019-04-26 09:13:57 +0000

    perl: converting bits to bytes and other units so on.

--- 2019-4-26-practice-perl-converting-bits-to-bytes.pl
+++ 2019-4-26-practice-perl-converting-bits-to-bytes.pl
@@ -74,3 +74,128 @@ $PARSERT->add_tag(
         return qq{<OPTION VALUE="$value"> $selected>};
     }
 );
+if ( !$input_units && $input_amount ) {
+    print $PARSER->parse_file();
+    exit;
+}
+
+my %output;
+my @units = qw( bits bytes kilobits kilobytes megabits
+    megabytes gigabits gigabytes terabytes petabytes
+);
+
+if ( "$input_units" eq 'bits' ) {    ## no critic - Would a hash be better?
+    $output{'bits'} = $input_amount;
+}
+elsif ( "$input_units" eq 'bytes' ) {
+    $output{'bits'} = $input_amount * $BITS_IN_A_BYTE;
+}
+elsif ( "$input_units" eq 'kilobits' ) {
+    $output{'bits'} = $input_amount * $kilo;
+}
+elsif ( "$input_units" eq 'kilobytes' ) {
+    $output{'bits'} = $input_amount * $BITS_IN_A_BYTE * $kilo;
+}
+elsif ( "$input_units" eq 'megabits' ) {
+    $output{'bits'} = $input_amount * $kilo * $kilo;
+}
+elsif ( "$input_units" eq 'megabytes' ) {
+    $output{'bits'} = $input_amount * $kilo * $kilo * $BITS_IN_A_BYTE;
+}
+elsif ( "$input_units" eq 'gigabits' ) {
+    $output{'bits'} = $input_amount * $kilo * $kilo * $kilo;
+}
+elsif ( "$input_units" eq 'gigabytes' ) {
+    $output{'bits'} = $input_amount * $kilo * $kilo * $kilo * $BITS_IN_A_BYTE;
+}
+elsif ( "$input_units" eq 'terabytes' ) {
+    $output{'bits'} = $input_amount * $kilo * $kilo * $kilo * $kilo * $BITS_IN_A_BYTE;
+}
+elsif ( "$input_units" eq 'terabits' ) {
+    $output{'bits'} = $input_amount * $kilo * $kilo * $kilo * $kilo;
+}
+elsif ( "$input_units" eq 'petabytes' ) {
+    $output{'bits'} = $input_amount * $kilo * $kilo * $kilo * $kilo * $kilo * $BITS_IN_A_BYTE;
+}
+elsif ( "$input_units" eq 'petabits' ) {
+    $output{'bits'} = $input_amount * $kilo * $kilo * $kilo * $kilo * $kilo;
+}
+else {
+    # uh oh
+    # shouldn't ever get here
+}
+
+$output{'bytes'}         = $output{'bits'} / $BITS_IN_A_BYTE;
+$output{'kilobits'}      = $output{'bits'} / $kilo,
+    $output{'kilobytes'} = $output{'kilobits'} / $BITS_IN_A_BYTE,
+    $output{'megabits'}  = $output{'kilobits'} / $kilo,
+    $output{'megabytes'} = $output{'megabits'} / $BITS_IN_A_BYTE,
+    $output{'gigabits'}  = $output{'megabits'} / $kilo,
+    $output{'gigabytes'} = $output{'gigabits'} / $BITS_IN_A_BYTE,
+    $output{'terabits'}  = $output{'gigabits'} / $kilo,
+    $output{'terabytes'} = $output{'terabits'} / $BITS_IN_A_BYTE,
+    $output{'petabits'}  = $output{'terabits'} / $kilo,
+    $output{'petabytes'} = $output{'petabits'} / $BITS_IN_A_BYTE,
+
+    $PARSER->list(@units);
+$PARSER->entry_file("$TEMPLATE_DIR/results.htmlf");
+
+$PARSER->entry_callback(
+    sub {
+        my $unit = shift;
+        my $tags = +{
+            UNIT   => "$unit",
+            AMOUNT => "$output{$unit}"
+        };
+        return $tags;
+    }
+);
+
+$PARSER->add_tags(
+    +{  RESULTS => $PARSER->parse_list_files,
+        AMOUNT  => "$input_amount",
+        UNITS   => "$input_units",
+    }
+);
+
+# Memoize it
+$cache_key = join q{:}, $kilo, $input_amount, $input_units;
+if ( !defined $CACHE{$cache_key} ) {
+    $CACHE{$cache_key} = $PARSER->parse_file;
+}
+
+print $CACHE{$cache_key};
+
+exit;
+
+################################################################################
+#  Utility subroutines
+################################################################################
+
+sub _get_notation_hash {
+    my $r = shift || Carp::confess('No CGI object passed.');
+    my %notation_info = (
+        message => '(informal notation: kilobyte = 1024 bytes)',
+        type    => 'legacy',
+        kilo    => 1024,                                           # legacy
+    );
+    my $notation = $r->param('notation') || 'legacy';
+    my $type = lc $notation;
+    return \%notation_info if ( $type eq 'legacy' );               # default
+    return \%notation_info if ( !defined $type );
+
+    if ( $type eq 'ieee' ) {
+        $notation_info{message} = '(IEEE notation: kilobyte = 1000 bytes)';
+        $notation_info{type}    = $type;
+        $notation_info{kilo}    = 1000;
+    }
+    return \%notation_info;
+}
+
+sub _print_source {
+    open my $fh, '<', "$ENV{SCRIPT_FILENAME}";
+    while (<$fh>) {
+        print;
+    }
+    return close $fh;
+}
