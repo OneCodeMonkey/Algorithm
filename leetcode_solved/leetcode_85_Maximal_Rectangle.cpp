dccd4c4ef9b6a285826129e23c39b2d505694907
OneCodeMonkey <1460018362@qq.com>
2019-07-08 14:35:53 +0800

    leetcode_85_Maximal_Rectangle

--- leetcode_solved/leetcode_85_Maximal_Rectangle.cpp
+++ leetcode_solved/leetcode_85_Maximal_Rectangle.cpp
@@ -9,6 +9,25 @@
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-     	
+     	if(matrix.empty() || matrix[0].empty())
+     		return 0;
+     	int rows = matrix.size();
+     	int cols = matrix[0].size();
+     	int ret = 0;
+     	vector<int>h(cols + 1);
+     	for(int i = 0; i < rows; i++) {
+     		stack<int>lowi;
+     		for(int j = 0; j <= cols; j++) {
+     			h[j] = ((j != cols && matrix[i][j] == '1') ? h[j] + 1 : 0);
+     			while(!lowi.empty() && (h[j] < h[lowi.top()])) {
+     				int height = h[lowi.top()];
+     				lowi.pop();
+     				int lefti = (lowi.empty() ? -1 : lowi.top());
+     				ret = max((j - lefti - 1) * height, ret);
+     			}
+     			lowi.push(j);
+     		}
+     	}
+     	return ret;
     }
 };
