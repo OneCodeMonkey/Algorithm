ad456642e87d06e7b0531e4358f12007327777a3
OneCodeMonkey <1460018362@qq.com>
2019-07-16 15:27:25 +0800

    Solution of O(n^3)

--- leetcode_solved/leetcode_16_3Sum_Closest.cpp
+++ leetcode_solved/leetcode_16_3Sum_Closest.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
+	/**
+	 * Original Solution which complexity is O(n^3)
+	 */
     int threeSumClosest(vector<int>& nums, int target) {
-        
+        int n = nums.size();
+        int diff = target;
+        int sum = nums[0] + nums[1] + nums[2];
+        for(int i = 0; i < n - 2; i++) {
+        	for(int j = i + 1; j < n - 1; j++) {
+        		for(int k = j + 1; k < n; k++) {
+        			if(Math.abs(nums[i] + nums[j] + nums[k] - target) < diff) {	// 出现更接近的组合
+        				sum = nums[i] + nums[j] + nums[k];
+        			}
+        		}
+        	}
+        }
     }
 };
