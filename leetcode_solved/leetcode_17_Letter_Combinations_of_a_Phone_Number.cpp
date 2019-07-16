fbe882d112281b4a7ffeb6fbcee903bf15b5157d
OneCodeMonkey <1460018362@qq.com>
2019-07-16 17:13:15 +0800

    leetcode_17_Letter_Combinations_of_a_Phone_Number

--- leetcode_solved/leetcode_17_Letter_Combinations_of_a_Phone_Number.cpp
+++ leetcode_solved/leetcode_17_Letter_Combinations_of_a_Phone_Number.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        
+        int len = strlen(digits);
+
+        // record how many alpha choices each number have.
+        int choiceCount[8] = {3, 3, 3, 3, 3, 4, 3, 4};
+
+        int alpha[8][4] = {
+        	{'a', 'b', 'c'},
+        	{'d', 'e', 'f'},
+        	{'g', 'h', 'i'},
+        	{'j', 'k', 'l'},
+        	{'m', 'n', 'o'},
+        	{'p', 'q', 'r', 's'},
+        	{'t', 'u', 'v'},
+        	{'w', 'x', 'y', 'z'},
+        };
+
+        vector<string> ret;
+
+        while(--len) {
+        	string str = '';
+        	
+        	
+        }
+
+        return ret;
     }
 };
