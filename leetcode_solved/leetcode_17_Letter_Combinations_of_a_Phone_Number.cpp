add98d76e7ad79779ba753799104021b61134def
OneCodeMonkey <1460018362@qq.com>
2019-07-17 20:08:13 +0800

    leetcode_17_Letter_Combinations_of_a_Phone_Number

--- leetcode_solved/leetcode_17_Letter_Combinations_of_a_Phone_Number.cpp
+++ leetcode_solved/leetcode_17_Letter_Combinations_of_a_Phone_Number.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        int len = strlen(digits);
+        int len = digits.length();
+        int sum = 1, a, b;
 
         // record how many alpha choices each number have.
         int choiceCount[8] = {3, 3, 3, 3, 3, 4, 3, 4};
@@ -18,11 +19,27 @@ public:
         };
 
         vector<string> ret;
+        vector<int>digit;	// 记录每一位，的可选字母有几个，比如 2 有 a,b,c 三个可选字母，那么对应是3
+        for(int i = 0; i < len; i++) {
+        	digit.push_back(choiceCount[digits[i] + 1]);
+        	sum *= choiceCount[digits[i] + 1];
+        }
 
-        while(--len) {
-        	string str = '';
-        	
-        	
+        for(int i = 1; i <= sum; i++) {
+        	string tmpStr = "";
+        	// 生成字符串。
+        	int pos = 0;
+        	a = i % digit[pos];
+        	b = i / digit[pos];
+        	tmpStr += alpha[digits[pos] - 1][a];
+        	while(b > 1) {
+        		pos++;
+        		a = b % digit[pos];
+        		b = b / digit[pos];
+        		tmpStr += alpha[digits[pos] - 1][a];
+        	}
+
+        	ret.push_back(tmpStr);
         }
 
         return ret;
