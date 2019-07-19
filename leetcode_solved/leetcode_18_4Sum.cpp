067321262262cb7aed902233497c40866c632290
OneCodeMonkey <1460018362@qq.com>
2019-07-19 14:01:07 +0800

    leetcode_18_4Sum: fix

--- leetcode_solved/leetcode_18_4Sum.cpp
+++ leetcode_solved/leetcode_18_4Sum.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
 	/**
+	 * AC: 
+	 * Runtime: 48 ms, faster than 30.52% of C++ online submissions for 4Sum.
+	 * Memory Usage: 10.6 MB, less than 11.68% of C++ online submissions for 4Sum.
+	 *
 	 * 思路：先排序，然后以 O(n^2) 的复杂度做遍历，每一轮用两个游标缩小范围。
 	 * 总体复杂度：O(n^2)
 	 */
@@ -26,15 +30,23 @@ public:
      					tempArr.push_back(nums[l]);
      					tempArr.push_back(nums[r]);
 
-     					// 去重
+     					// 去重, 这里尝试直接 it == tempArr，结果不是想要的效果，即使值一样由于指针不一样也会判定为不一样。所以挨个比较值
+     					// 这里也体现出 C++ 的语言不方便性，换其他语言比如 php，直接 一个 exist() 就能完成的事。
+                        int flag = 0;
      					for(auto it:ret) {
-     						if(it == tempArr) {
-     							continue;
+     						if(it[0] == tempArr[0] && it[1] == tempArr[1] && it[2] == tempArr[2] && it[3] == tempArr[3]) {
+     							flag = 1;
+                                break;
      						}
      					}
+                        if(flag) {
+                            l++;
+                            continue;
+                        }
 
      					ret.push_back(tempArr);
-                        break;
+                        // break;       // 这里不应该直接退出，因为第三四个有可能有多种满足的组合
+                        l++;
      				} else if(tempSum > target)
      					r--;
      				else if(tempSum < target)
