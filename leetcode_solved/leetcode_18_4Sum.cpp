13035a57dc379fd23c5001cee00df36fe6e0b743
OneCodeMonkey <1460018362@qq.com>
2019-07-19 12:30:18 +0800

    [editing]leetcode_18_4Sum

--- leetcode_solved/leetcode_18_4Sum.cpp
+++ leetcode_solved/leetcode_18_4Sum.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
+	/**
+	 * 思路：先排序，然后以 O(n^2) 的复杂度做遍历，每一轮用两个游标缩小范围。
+	 * 总体复杂度：O(n^2)
+	 */
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        
+     	vector<vector<int> >ret;
+     	   
     }
 };
