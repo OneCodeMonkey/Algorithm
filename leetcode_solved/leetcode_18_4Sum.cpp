45b719dfa7bef4392f9c8872720ac3b092c334d6
OneCodeMonkey <1460018362@qq.com>
2019-07-19 13:46:34 +0800

    fix

--- leetcode_solved/leetcode_18_4Sum.cpp
+++ leetcode_solved/leetcode_18_4Sum.cpp
@@ -6,6 +6,43 @@ public:
 	 */
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
      	vector<vector<int> >ret;
-     	   
+     	int size = nums.size();
+     	if(size < 4)
+     		return vector<vector<int> >();
+
+     	// sort
+     	sort(nums.begin(), nums.end());
+
+     	for(int i = 0; i < size - 1; i++) {
+     		for(int j = i + 1; j < size; j++) {
+     			int l = j + 1;
+     			int r = size - 1;
+     			while(l < r) {
+     				int tempSum = nums[i] + nums[j] + nums[l] + nums[r];
+     				if(tempSum == target) {
+     					vector<int>tempArr;
+     					tempArr.push_back(nums[i]);
+     					tempArr.push_back(nums[j]);
+     					tempArr.push_back(nums[l]);
+     					tempArr.push_back(nums[r]);
+
+     					// 去重
+     					for(auto it:ret) {
+     						if(it == tempArr) {
+     							continue;
+     						}
+     					}
+
+     					ret.push_back(tempArr);
+                        break;
+     				} else if(tempSum > target)
+     					r--;
+     				else if(tempSum < target)
+     					l++;
+     			}
+     		}
+     	}
+
+     	return ret;
     }
 };
