465f9c105d80a56e38874abc45a4e71b8cee5dbc
OneCodeMonkey <1460018362@qq.com>
2019-07-22 01:43:51 +0800

    leetcode_1130_Minimum_Cost_Tree_From_Leaf_Values

--- leetcode_solved/leetcode_1130_Minimum_Cost_Tree_From_Leaf_Values.cpp
+++ leetcode_solved/leetcode_1130_Minimum_Cost_Tree_From_Leaf_Values.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     int mctFromLeafValues(vector<int>& arr) {
-
+    	int n = arr.size();
+    	vector<vector<pair<int,int> > > dp(n, vector<pair<int,int> > (n));
+    	for(int len = 0; len < n; len++) {
+    		for(int i = 0; i < n - len; i++) {
+    			int j = i + len;
+    			if(i == j) {
+    				dp[i][j].first = 0;
+    				dp[i][j].second = arr[i];
+    				continue;
+    			}
+    			dp[i][j].first = INT_MAX;
+    			dp[i][j].second = 0;
+    			for(int k = i; k < j; k++) {
+    				dp[i][j].second = max(dp[i][k].second, dp[k + 1][j].second);
+    				int sum = dp[i][k].second * dp[k + 1][j].second;
+    				sum += dp[i][k].first + dp[k + 1][j].first;
+    				if(dp[i][j].first > sum)
+    					dp[i][j].first = sum;
+    			}
+    		}
+    	}
+    	return dp[0][n - 1].first;	
     }
 };
