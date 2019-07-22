84109e57d1b818548e2f29163f503117713dab9b
OneCodeMonkey <1460018362@qq.com>
2019-07-22 17:37:31 +0800

    add annotation

--- leetcode_solved/leetcode_1129_Shortest_Path_with_Alternating_Color.cpp
+++ leetcode_solved/leetcode_1129_Shortest_Path_with_Alternating_Color.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+	/**
+	 * 思路：题目大意，给出一个n个点的有向图，其中的有向边要么是红色，要么是蓝色。
+	 * 欲求数组 a[n]，表示从节点0到 k (0 <= k <= n-1) 的最短路径的长度，且每条最短路径上的边都必须是红蓝交替的形式。如果不存在
+	 * 这样的最短路径，则 a[k] = -1.
+	 */
     vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& red_edges, vector<vector<int>>& blue_edges) {
         
     }
