3899f72388ce0a6f1a8b1a4a69ed96c77a96499d
OneCodeMonkey <1460018362@qq.com>
2019-07-24 19:48:20 +0800

    leetcode_1124_Longest_Well-Performing_Interval

--- leetcode_solved/leetcode_1124_Longest_Well-Performing_Interval.cpp
+++ leetcode_solved/leetcode_1124_Longest_Well-Performing_Interval.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+	/**
+	 * 思路：数组先按照 hours[i] > 8 ? 1 : -1 转换成一个 0，1 数组
+	 * 然后我们来一个两重for 循环，但是会进行一些判断，来跳过大量的不需要考虑的清醒。
+	 * 具体如何对此进行剪支的，详细见下面代码。
+	 */
     int longestWPI(vector<int>& hours) {
         
     }
