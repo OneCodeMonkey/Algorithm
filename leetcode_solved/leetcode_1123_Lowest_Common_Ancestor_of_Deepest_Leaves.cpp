dfcc4cf3381ed6726f447bc546a8d45df41d9e17
OneCodeMonkey <1460018362@qq.com>
2019-07-27 15:58:13 +0800

    leetcode_1123_Lowest_Common_Ancestor_of_Deepest_Leaves

--- leetcode_solved/leetcode_1123_Lowest_Common_Ancestor_of_Deepest_Leaves.cpp
+++ leetcode_solved/leetcode_1123_Lowest_Common_Ancestor_of_Deepest_Leaves.cpp
@@ -8,8 +8,34 @@
  * };
  */
 class Solution {
+	int retd;
+	TreeNode* ret;
+	int getDepth(TreeNode* root, int h) {
+		if(!root->left && !root->right) {
+			if(h > retd) {
+				retd = h;
+				ret = root;
+			}
+			return h;
+		}
+		int ll = 0;
+		int rr = 0;
+		if(root->left)
+			ll = getDepth(root->left, h + 1);
+		if(root->right)
+			rr = getDepth(root->right, h + 1);
+		if(ll == rr && ll == retd)
+			ret = root;
+
+		return max(ll, rr);
+	}
 public:
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        
+        retd = -1;
+        ret = NULL;
+        if(root)
+        	getDepth(root, 0);
+
+        return ret;
     }
 };
