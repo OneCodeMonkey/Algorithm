1861ac5232243f9853de8f2fd3a3f8915e601544
OneCodeMonkey <1460018362@qq.com>
2019-07-28 11:13:17 +0800

    contest_2: const define

--- leetcode_solved/leetcode_1138_Alphabet_Board_Path.cpp
+++ leetcode_solved/leetcode_1138_Alphabet_Board_Path.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     string alphabetBoardPath(string target) {
-        
+        vector<string> board = {
+        	"abcde",
+        	"fghij",
+        	"klmno",
+        	"pqrst",
+        	"uvwxy",
+        	"z"
+        };
     }
 };
