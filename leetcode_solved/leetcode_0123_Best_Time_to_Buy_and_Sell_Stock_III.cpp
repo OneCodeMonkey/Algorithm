7dfa8fc141cd1aa03b3b2cbcf9350073c97243b2
OneCodeMonkey <1460018362@qq.com>
2019-07-30 22:42:23 +0800

    leetcode_0123_Best_Time_to_Buy_and_Sell_Stock_III: fix

--- leetcode_solved/leetcode_0123_Best_Time_to_Buy_and_Sell_Stock_III.cpp
+++ leetcode_solved/leetcode_0123_Best_Time_to_Buy_and_Sell_Stock_III.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-   		vector<int>     
+   		int n = prices.size();
+   		vector<int> lmax(n, 0);
+   		vector<int> rmax(n, 0);
+
+   		int minT = INT_MAX;
+   		int tempMax = 0;
+   		for(int i = 0; i < n; i++) {
+   			if(prices[i] < minT) {
+   				minT = prices[i];
+   			} else {
+   				tempMax = max(tempMax, prices[i] - minT);
+   			}
+   			lmax[i] = tempMax;
+   		}
+   		int maxT = INT_MIN;
+   		tempMax = 0;
+   		for(int i = n - 1; i >= 0; i--) {
+   			if(prices[i] > maxT) {
+   				maxT = prices[i];
+   			} else {
+   				tempMax = max(tempMax, maxT - prices[i]);
+   			}
+   			rmax[i] = tempMax;
+   		}
+
+   		int ans = 0;
+   		for(int i = 0; i < n; i++)
+   			ans = max(ans, lmax[i] + rmax[i]);
+
+   		return ans;
     }
 };
