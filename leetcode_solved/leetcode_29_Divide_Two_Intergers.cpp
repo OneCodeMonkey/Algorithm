43c72e59a4d3c7858b9b03a0eab902165fb5b7e8
OneCodeMonkey <1460018362@qq.com>
2019-08-01 15:09:08 +0800

    add annotation

--- leetcode_solved/leetcode_29_Divide_Two_Intergers.cpp
+++ leetcode_solved/leetcode_29_Divide_Two_Intergers.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+	/**
+	 * AC:
+	 * Runtime: 4 ms, faster than 80.61% of C++ online submissions for Divide Two Integers.
+	 * Memory Usage: 8.2 MB, less than 77.72% of C++ online submissions for Divide Two Integers.
+     *
+     */
     int divide(int dividend, int divisor) {
         if(dividend == -2147483648 && divisor == -1)
             return 2147483647;
