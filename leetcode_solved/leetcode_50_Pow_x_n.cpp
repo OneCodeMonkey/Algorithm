914c2cda8544c62e4e36bb267bb6c42c98c6caac
OneCodeMonkey <1460018362@qq.com>
2019-08-01 16:13:47 +0800

    leetcode_50_Pow_x_n

---
+++ leetcode_solved/leetcode_50_Pow_x_n.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    double pow(double x, long long n) {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return x;
+        double value = pow(x, n / 2);
+        value = value * value;
+        if (n % 2) {
+            value = value * x;
+        }
+
+        return value;
+    }
+    double myPow(double x, int n) {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return x;
+        double ans;
+        if (n < 0) {
+            n = -n;
+            x = 1 / x;
+        }
+        ans = pow(x, n);
+        return ans;
+    }
+};
