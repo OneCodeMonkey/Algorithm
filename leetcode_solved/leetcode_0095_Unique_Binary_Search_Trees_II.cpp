cc5ad8085f8bfb8b61bb11792f8d441e58f86368
OneCodeMonkey <1460018362@qq.com>
2019-08-01 11:07:44 +0800

    leetcode_0095_Unique_Binary_Search_Trees_II

--- leetcode_solved/leetcode_0095_Unique_Binary_Search_Trees_II.cpp
+++ leetcode_solved/leetcode_0095_Unique_Binary_Search_Trees_II.cpp
@@ -9,7 +9,39 @@
  */
 class Solution {
 public:
+	vector<TreeNode*> generateTrees(int s, int t) {
+		vector<TreeNode*> ans;
+		if(s > t) {
+			ans.push_back(NULL);
+			return ans; 
+		}
+		if(s == t) {
+			TreeNode* node1 = new TreeNode(s);
+			ans.push_back(node1);
+			return ans;
+		}
+		for(int i = s; i <= t; i++) {
+			vector<TreeNode*> l = generateTrees(s, i - 1);
+			vector<TreeNode*> r = generateTrees(i + 1, t);
+			for(auto it1:l) {
+				for(auto it2:r) {
+					TreeNode* node2 = new TreeNode(i);
+					node2->left = it1;
+					node2->right = it2;
+					ans.push_back(node2);
+				}
+			}
+		}
+
+		return ans;
+	}
     vector<TreeNode*> generateTrees(int n) {
-        
+     	vector<TreeNode*> ans;
+     	if(n == 0) {
+     		ans.push_back(NULL);
+     		return ans;   
+     	}
+
+     	return generateTrees(1, n);
     }
 };
