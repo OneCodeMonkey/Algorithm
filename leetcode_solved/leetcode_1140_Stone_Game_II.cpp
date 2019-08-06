4555ad5f22e08404725490d4af3d65e526bc0f03
OneCodeMonkey <1460018362@qq.com>
2019-08-06 10:00:19 +0800

    leetcode_1140_Stone_Game_II

--- leetcode_solved/leetcode_1140_Stone_Game_II.cpp
+++ leetcode_solved/leetcode_1140_Stone_Game_II.cpp
@@ -1,7 +1,28 @@
 class Solution {
 public:
+	int solve(int record[101], int memo[101][101], int start, int M, int border) {
+		if(start >= border)
+			return 0;
+		else if(memo[start][M] > 0)
+			return memo[start][M];
+		else {
+			for(int i = 1; i <= 2 * M && start + i <= border; i++) {
+				memo[start][M] = max(memo[start][M], record[start] - solve(record, memo, i + start, max(M, i), border));
+			}
+			return memo[start][M];
+		}
+	}
     int stoneGameII(vector<int>& piles) {
-        vector<int> ret;
+        int n = piles.size();
+        int memo[101][101] = {0};
+        int record[101] = {0};
+        record[n - 1] = piles[n - 1];
+        for(int i = n - 2; i >= 0; i--) {
+        	record[i] = record[i + 1] + piles[i];
+        }
+
+        return solve(record, memo, 0, 1, n);
         
     }
 };
+
