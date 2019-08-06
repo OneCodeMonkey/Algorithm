df0857b3a76db05857d1f7c7bef7c86fab13916b
OneCodeMonkey <1460018362@qq.com>
2019-08-06 12:11:12 +0800

    leetcode_0115_Distinct_Subsequences

--- leetcode_solved/leetcode_0115_Distinct_Subsequences.cpp
+++ leetcode_solved/leetcode_0115_Distinct_Subsequences.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        
+     	const size_t S = s.size();
+     	const size_t T = t.size();
+     	if(!S || !T)
+     		return 0;
+     	vector<uint32_t> dp(T + 1, 0); 
+     	dp[0] = 1;
+     	for(size_t i = 0; i < S; i++)
+     		for(size_t j = T; j >0; j--)
+     			if(t[j - 1] == s[i])
+     				dp[j] += dp[j - 1];
+
+     	return dp[T];
     }
 };
