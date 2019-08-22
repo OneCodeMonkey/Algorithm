e2139dde44a9347e0abca7d6fead1d46d4a56b74
OneCodeMonkey <1460018362@qq.com>
2019-08-22 11:43:24 +0800

    leetcode_1092_Shortest_Common_Supersequence

--- leetcode_solved/leetcode_1092_Shortest_Common_Supersequence.cpp
+++ leetcode_solved/leetcode_1092_Shortest_Common_Supersequence.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        
+        int i = 0, j = 0;
+        string res = "";
+        for(char c:lcs(str1, str2)) {
+        	while(str1[i] != c)
+        		res += str1[i++];
+        	while(str2[j] != c)
+        		res += str2[j++];
+        	res += c;
+        	i++;
+        	j++;
+        }
+
+        return res + str1.substr(i) + str2.substr(j);
+    }
+
+    string lcs(string& A, string& B) {
+    	int n = A.size();
+    	int m = B.size();
+    	vector<vector<string> >dp(n + 1, vector<string>(m + 1, ""));
+    	for(int i = 0; i < n; i++)
+    		for(int j = 0; j < m; j++)
+    			if(A[i] == B[j])
+    				dp[i + 1][j + 1] = dp[i][j] + A[i];
+    			else
+    				dp[i + 1][j + 1] = dp[i + 1][j].size() > dp[i][j + 1].size() ? dp[i + 1][j] : dp[i][j + 1];
+
+		return dp[n][m];    			
     }
 };
