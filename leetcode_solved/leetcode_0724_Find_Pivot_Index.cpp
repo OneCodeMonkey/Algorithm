c3734d580aa16a692c1d360fbd6f4d98920a5eb4
OneCodeMonkey <1460018362@qq.com>
2019-09-06 21:22:34 +0800

    leetcode_0724_Find_Pivot_Index

--- leetcode_solved/leetcode_0724_Find_Pivot_Index.cpp
+++ leetcode_solved/leetcode_0724_Find_Pivot_Index.cpp
@@ -1,6 +1,29 @@
+/**
+ * 思路：先求和。然后在 i 位置，判断 [0, i] 的和是否等于 (sum + a[i]) / 2
+ *
+ */
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        
+    	int size = nums.size();
+    	if(size == 0)
+    		return -1;
+    	if(size == 1)
+    		return 0;
+
+    	int sum = 0;
+        for(auto num:nums) {
+        	sum+= num;
+        }
+
+        int sumK = nums[0];		// 第0到i位的和
+        for(int i = 0; i < size; i++) {
+        	if(i !=0)
+        		sumK += nums[i];
+        	if(sumK * 2 == sum + nums[i])
+        		return i;
+        }
+
+        return -1;
     }
 };
