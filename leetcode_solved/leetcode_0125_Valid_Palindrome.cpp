cb799a02831ea8f7a902ce312aa9ba24db67276d
OneCodeMonkey <1460018362@qq.com>
2019-09-06 21:11:35 +0800

    fix

--- leetcode_solved/leetcode_0125_Valid_Palindrome.cpp
+++ leetcode_solved/leetcode_0125_Valid_Palindrome.cpp
@@ -1,4 +1,11 @@
-// 简单题，注意判断数字的情况，example 中并未给数字的情况，但要注意到这种情形
+/**
+ * AC:
+ * Runtime: 8 ms, faster than 84.27% of C++ online submissions for Valid Palindrome.
+ * Memory Usage: 10.7 MB, less than 6.12% of C++ online submissions for Valid Palindrome.
+ * 简单题，注意判断数字的情况，example 中并未给数字的情况，但要注意到这种情形
+ * 大小写做下转换，使其ASCII码数字上 大小写相等。纯数字直接用原 ASCII 码
+ *
+ */
 class Solution {
 public:
     bool isPalindrome(string s) {
@@ -8,12 +15,12 @@ public:
 		for (auto charItem : s) {
 			int temp = charItem - 'a';
 			int temp2 = charItem - 'A';
-			int temp3 = charItem - '0';
+			int temp3 = charItem;
 			if ((temp >= 0 && temp <= 25))
 				vec1.push_back(temp);
 			if ((temp2 >= 0 && temp2 <= 25))
 				vec1.push_back(temp2);
-			if ((temp3 >= 0 && temp3 <= 10))
+			if ((temp3 >= 48 && temp3 <= 57))
 				vec1.push_back(temp3);
 		}
 
