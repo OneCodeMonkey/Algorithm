cb8d9077f0e825a2925091386df360a16f5abdbf
OneCodeMonkey <1460018362@qq.com>
2019-09-06 21:58:12 +0800

    AC: leetcode_0669_Trim_a_Binary_Search_Tree

--- leetcode_solved/leetcode_0669_Trim_a_Binary_Search_Tree.cpp
+++ leetcode_solved/leetcode_0669_Trim_a_Binary_Search_Tree.cpp
@@ -1,4 +1,8 @@
 /**
+ * 思路：二叉搜索树删减元素，使剩余的元素都能落在给定的区间以内
+ * 递归判断 root 的值与 [L, R] 的关系。
+ */
+/**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
@@ -10,6 +14,15 @@
 class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int L, int R) {
-        
+        if(root == NULL)
+        	return root;
+        if(root->val < L)	// 只可能在右子树
+        	return trimBST(root->right, L, R);
+        if(root->val > R)	// 只可能在左子树
+        	return trimBST(root->left, L, R);
+        root->left = trimBST(root->left, L, R);		// 递归向下判断
+        root->right = trimBST(root->right, L, R);
+
+        return root;
     }
 };
