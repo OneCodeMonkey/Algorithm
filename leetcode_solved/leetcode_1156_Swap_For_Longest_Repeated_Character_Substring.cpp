7135b07255de96bdb090b4b8e30d62a3cdd40c4e
OneCodeMonkey <1460018362@qq.com>
2019-09-06 20:35:19 +0800

    leetcode_1156_Swap_For_Longest_Repeated_Character_Substring

--- leetcode_solved/leetcode_1156_Swap_For_Longest_Repeated_Character_Substring.cpp
+++ leetcode_solved/leetcode_1156_Swap_For_Longest_Repeated_Character_Substring.cpp
@@ -1,11 +1,29 @@
 /**
- * 思路：三个以上不同字母同时存在，则这个一定不行。
- * 
+ * AC:
+ * Runtime: 8 ms, faster than 71.54% of C++ online submissions for Swap For Longest Repeated Character Substring.
+ * Memory Usage: 9.8 MB, less than 100.00% of C++ online submissions for Swap For Longest Repeated Character Substring.
  *
  */
 class Solution {
 public:
     int maxRepOpt1(string text) {
-     	   
+    	int ans = 1;
+     	vector<vector<int>>idx(26);
+     	for(int i = 0; i < text.size(); i++)
+     		idx[text[i] - 'a'].push_back(i);
+     	for(int m = 0; m < 26; m++) {
+     		int cnt = 1, cnt1 = 0, mx = 0;
+     		for(int n = 1; n < idx[m].size(); n++) {
+     			if(idx[m][n] == idx[m][n - 1] + 1)
+     				cnt++;
+     			else {
+     				cnt1 = idx[m][n] == idx[m][n - 1] + 2 ? cnt : 0;
+     				cnt = 1;
+     			}
+     			mx = max(mx, cnt1 + cnt);
+     		}
+     		ans = max(ans, mx + (idx[m].size() > mx ? 1 : 0));
+     	}
+     	return ans;
     }
 };
