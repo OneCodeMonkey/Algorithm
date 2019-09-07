51ec231a834610e6e5c9ffb2ee39bdeb8ce323e6
OneCodeMonkey <1460018362@qq.com>
2019-09-07 21:43:01 +0800

    1177. Can Make Palindrome from Substring

--- leetcode_solved/[editing]leetcode_5175_Can_Make_Palindrome_from_Substring.cpp
+++ leetcode_solved/[editing]leetcode_5175_Can_Make_Palindrome_from_Substring.cpp
@@ -2,6 +2,28 @@ class Solution {
 public:
     vector<bool> canMakePaliQueries(string s, vector<vector<int>>& queries) {
         vector<bool> ret;
-        
+        vector<vector<int>>vec;
+
+        vector<int>tmp(26, 0);
+        vec.push_back(tmp);
+
+        for(int i = 0; i < s.size(); i++) {
+        	tmp[s[i] - 'a']++;
+        	vec.push_back(tmp);
+        }
+
+        for(auto const& q:queries) {
+        	int low = q[0], high = q[1], k = q[2];
+        	vector<int> vec2 = vec[high + 1];
+        	int odds = 0, sum = 0;
+        	for(int i = 0; i < 26; i++) {
+        		vec2[i] -= vec[low][i];
+        		sum += vec2[i];
+        		odds += (vec2[i] % 2);
+        	}
+
+        	ret.push_back((odds -= (sum % 2)) <= k * 2 ? true : false);
+        }
+        return ret;
     }
 };
