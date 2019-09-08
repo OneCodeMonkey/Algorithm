085f44e9d623bcec99ae6963b4eee92336c13028
OneCodeMonkey <1460018362@qq.com>
2019-09-08 22:29:18 +0800

    leetcode_1185_Day_of_the_Week

--- leetcode_solved/leetcode_1185_Day_of_the_Week.cpp
+++ leetcode_solved/leetcode_1185_Day_of_the_Week.cpp
@@ -1,4 +1,8 @@
 /**
+ * AC: 最快的解法
+ * Runtime: 0 ms, faster than 100.00% of C++ online submissions for Day of the Week.
+ * Memory Usage: 8 MB, less than 100.00% of C++ online submissions for Day of the Week.
+ *
  * 思路：网上查到一个快捷公式。否则处理闰年的情况，代码会写很长。。这里偷鸡一下 :(
  * 基姆拉尔森计算公式
  * W= (d+2*m+3*(m+1)/5+y+y/4-y/100+y/400+1) mod 7
