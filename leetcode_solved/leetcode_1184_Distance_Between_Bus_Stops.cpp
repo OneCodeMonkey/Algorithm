9507151562c6e193e5e73602c793e0e82da1f82c
OneCodeMonkey <1460018362@qq.com>
2019-09-09 17:15:24 +0800

    leetcode_1184_Distance_Between_Bus_Stops

--- leetcode_solved/leetcode_1184_Distance_Between_Bus_Stops.cpp
+++ leetcode_solved/leetcode_1184_Distance_Between_Bus_Stops.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
-        
-    }
+		vector<int> sumK;	// 计算 i 位置到 0 的顺次出发的路径距离
+		int sum = 0;
+		sumK.push_back(0);
+
+		for (int i = 0; i < distance.size(); i++)
+			sum += distance[i];
+
+		for (int i = 0; i < distance.size() - 1; i++) {
+			sumK.push_back(sumK[i] + distance[i]);
+		}
+
+		return min(abs(sumK[destination] - sumK[start]), sum - abs(sumK[destination] - sumK[start]));
+	}
 };
