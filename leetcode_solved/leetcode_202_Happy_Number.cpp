f5b0ce82003635ceb057442dac4073558150de90
OneCodeMonkey <1460018362@qq.com>
2019-09-09 16:50:58 +0800

    fix

--- leetcode_solved/leetcode_202_Happy_Number.cpp
+++ leetcode_solved/leetcode_202_Happy_Number.cpp
@@ -2,26 +2,35 @@ class Solution {
 public:
 	int doOperation(int n) {
 		int ret = 0;
-		while(n) {
+		while (n) {
 			ret += (n % 10) * (n % 10);
 			n = n / 10;
 		}
 		return ret;
 	}
-    bool isHappy(int n) {
-     	int temp = doOperation(n);
-     	int temp2;
-     	if(temp == 1)
-     		return true;
-     	while(temp != 1) {
-     		temp2 = doOperation(n);
-     		if(temp2 == 1)
-     			return true;
-     		if(temp2 == temp)
-     			return false;
+	bool isHappy(int n) {
+		int temp = doOperation(n);
+		int temp2;
+		vector<int> record;
+		if (temp == 1)
+			return true;
+		record.push_back(temp);
 
-     		temp = temp2;
-     	}
-     	return false;
-    }
+		while (temp != 1) {
+			temp2 = doOperation(temp);
+			if (temp2 == 1)
+				return true;
+
+			vector<int>::iterator ret;
+			ret = std::find(record.begin(), record.end(), temp2);
+			
+			if (ret != record.end())	// 记录是否进入无法跳出的循环
+				return false;
+
+			record.push_back(temp2);
+
+			temp = temp2;
+		}
+		return false;
+	}
 };
