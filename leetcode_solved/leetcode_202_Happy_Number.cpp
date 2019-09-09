c26e1b1e9a46a7c19f0279c84e049d5b5eab0f97
OneCodeMonkey <1460018362@qq.com>
2019-09-09 16:52:42 +0800

    add annotation

--- leetcode_solved/leetcode_202_Happy_Number.cpp
+++ leetcode_solved/leetcode_202_Happy_Number.cpp
@@ -1,3 +1,12 @@
+/**
+ * AC:
+ * Runtime: 4 ms, faster than 66.37% of C++ online submissions for Happy Number.
+ * Memory Usage: 8.3 MB, less than 88.46% of C++ online submissions for Happy Number.
+ * 思路：很直白的题。但注意判断何种条件下会进入无法跳出的循环计算中。
+ * 用 vector 记录无法得1时，每次的历史值。只要当前一次操作后的值等于某次
+ * 历史值，则可以说明已经进入死循环。这是充分条件。
+ *
+ */
 class Solution {
 public:
 	int doOperation(int n) {
@@ -23,7 +32,7 @@ public:
 
 			vector<int>::iterator ret;
 			ret = std::find(record.begin(), record.end(), temp2);
-			
+
 			if (ret != record.end())	// 记录是否进入无法跳出的循环
 				return false;
 
