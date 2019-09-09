0e74e45670a9c1ac8c06beac3b2c965437aa3006
OneCodeMonkey <1460018362@qq.com>
2019-09-09 16:44:28 +0800

    first try

--- leetcode_solved/leetcode_202_Happy_Number.cpp
+++ leetcode_solved/leetcode_202_Happy_Number.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
+	int doOperation(int n) {
+		int ret = 0;
+		while(n) {
+			ret += (n % 10) * (n % 10);
+			n = n / 10;
+		}
+		return ret;
+	}
     bool isHappy(int n) {
-        
+     	int temp = doOperation(n);
+     	int temp2;
+     	if(temp == 1)
+     		return true;
+     	while(temp != 1) {
+     		temp2 = doOperation(n);
+     		if(temp2 == 1)
+     			return true;
+     		if(temp2 == temp)
+     			return false;
+
+     		temp = temp2;
+     	}
+     	return false;
     }
 };
