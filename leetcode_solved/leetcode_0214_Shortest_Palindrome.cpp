80a6075dc066092d0a2d8bb14702389a7c2a56ff
OneCodeMonkey <1460018362@qq.com>
2019-09-11 02:56:07 +0800

    leetcode_0214_Shortest_Palindrome

--- leetcode_solved/leetcode_0214_Shortest_Palindrome.cpp
+++ leetcode_solved/leetcode_0214_Shortest_Palindrome.cpp
@@ -1,31 +1,52 @@
+/**
+ * Brute-force can't pass...
+ *
+ */
 class Solution {
 public:
+    // string shortestPalindrome(string s) {
+    //     int size = s.size();
+    //     if(size == 0) {
+    //     	string s = "";
+    //     	return s;
+    //     }
+
+    //     int maxPos = 0;
+    //     for(int i = 0; i < size; i++) {
+    //     	bool flag = true;
+    //     	for(int j = 0; j < i / 2; j++) {
+    //     		if(s[j] != s[i - j]) {
+    //     			flag = false;
+    //     			break;
+    //     		}
+    //     	}
+    //     	if(flag)
+    //     		maxPos = i;
+    //     }
+
+    //     string ret = "";
+    //     for(int i = size - 1; i > maxPos; i++)
+    //     	ret += s[i];
+
+    //     ret += s;
+
+    //     return ret;
+    // }
+
     string shortestPalindrome(string s) {
-        int size = s.size();
-        if(size == 0) {
-        	string s = "";
-        	return s;
-        }
+    	string rev_s = s;
+    	reverse(rev_s.begin(), rev_s.end());
+    	string l = s + "#" + rev_s;
 
-        int maxPos = 0;
-        for(int i = 0; i < size; i++) {
-        	bool flag = true;
-        	for(int j = 0; j < i / 2; j++) {
-        		if(s[j] != s[i - j]) {
-        			flag = false;
-        			break;
-        		}
-        	}
-        	if(flag)
-        		maxPos = i;
-        }
+    	vector<int> p(l.size(), 0);
+    	for(int i = 1; i < l.size(); i++) {
+    		int j = p[i - 1];
+    		while(j > 0 && l[i] != l[j])
+    			j = p[j - 1];
 
-        string ret = "";
-        for(int i = size - 1; i > maxPos; i++)
-        	ret += s[i];
+    		p[i] = (j += l[i] == l[j]);
+    	}
 
-        ret += s;
-
-        return ret;
+    	return rev_s.substr(0, s.size() - p[l.size() - 1]) + s;
     }
 };
