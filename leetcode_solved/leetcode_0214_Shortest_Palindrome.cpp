2e40754599ae4c129f1028b2c3460789cf79fcdf
OneCodeMonkey <1460018362@qq.com>
2019-09-10 13:29:01 +0800

    brute-force try

--- leetcode_solved/leetcode_0214_Shortest_Palindrome.cpp
+++ leetcode_solved/leetcode_0214_Shortest_Palindrome.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        
+        int size = s.size();
+        if(size == 0) {
+        	string s = "";
+        	return s;
+        }
+
+        int maxPos = 0;
+        for(int i = 0; i < size; i++) {
+        	bool flag = true;
+        	for(int j = 0; j < i / 2; j++) {
+        		if(s[j] != s[i - j]) {
+        			flag = false;
+        			break;
+        		}
+        	}
+        	if(flag)
+        		maxPos = i;
+        }
+
+        string ret = "";
+        for(int i = size - 1; i > maxPos; i++)
+        	ret += s[i];
+
+        ret += s;
+
+        return ret;
     }
 };
