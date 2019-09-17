30d435b7ed99cfeb0c0eb26184b1d1208e6efd1b
OneCodeMonkey <1460018362@qq.com>
2019-09-18 02:02:33 +0800

    fix

--- leetcode_solved/[editing]leetcode_0058_Length_of_Last_Word.cpp
+++ leetcode_solved/[editing]leetcode_0058_Length_of_Last_Word.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        
+    	int length = 0, lastWordLength = 0;
+        for(int i = 0; i < s.length(); i++) {
+        	if(s[i] == ' ') {
+        		length = 0;
+        		continue;
+        	} else {
+        		length++;
+        		lastWordLength = length;
+        	}
+        }
+
+        return lastWordLength;
     }
 };
