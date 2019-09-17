094fecbb3be40b470505fda661a6fa99e241288d
OneCodeMonkey <1460018362@qq.com>
2019-09-18 02:05:29 +0800

    add annotation

--- leetcode_solved/[editing]leetcode_0058_Length_of_Last_Word.cpp
+++ leetcode_solved/[editing]leetcode_0058_Length_of_Last_Word.cpp
@@ -1,3 +1,10 @@
+/**
+ * AC:
+ * Runtime: 4 ms, faster than 70.73% of C++ online submissions for Length of Last Word.
+ * Memory Usage: 8.7 MB, less than 91.67% of C++ online submissions for Length of Last Word.
+ * 思路：注意最后一个词是最后一个不包含空格的最长子字符串。比如 "b aaba   " 这种，"aaba" 就是last_word
+ *
+ */
 class Solution {
 public:
     int lengthOfLastWord(string s) {
