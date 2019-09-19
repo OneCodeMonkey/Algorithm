c7a22b0699680c2f13953d38befbe26494411a53
OneCodeMonkey <1460018362@qq.com>
2019-09-19 12:28:06 +0800

    [editing]

--- leetcode_solved/leetcode_0067_Add_Binary.cpp
+++ leetcode_solved/leetcode_0067_Add_Binary.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        
+        int flag = 1;
+        vector<char> ans, avec, bvec;
+        for(int i = 0; i < a.length(); i++)
+        	avec.push_back(a[i]);
+        for(int i = 0; i < b.length(); i++)
+        	avec.push_back(b[i]);
+
+        int range = avec.size() < bvec.size() ? avec.size() : bvec.size();
+
+        for(int i = range - 1; i >= 0; i--) {
+        	// if()
+        }
     }
 };
