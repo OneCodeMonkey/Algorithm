13c5aa607f2d43668ecbbc8ed13b85bf57fa1978
OneCodeMonkey <1460018362@qq.com>
2019-09-19 12:18:28 +0800

    another way

--- leetcode_solved/leetcode_0066_Plus_One.cpp
+++ leetcode_solved/leetcode_0066_Plus_One.cpp
@@ -3,28 +3,23 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         int size = digits.size();
         vector<int> ans;
-        if(size == 1 && digits[0] == 0) {
-        	ans.push_back(1);
-        	return ans;
-        }
-
-        long long raw = 0, base = 1;
+        
+        int flag = 1;	// 是否需要进位
         for(int i = size - 1; i >= 0; i--) {
-        	raw += digits[i] * base;
-        	if(i != 0)		// 防止 base 溢出
-        		base *= 10;
-        }
-        long long plusNum = raw + 1;
-        vector<int> digit2;
-        while(plusNum > 0) {
-        	digit2.push_back(plusNum % 10);
-        	plusNum /= 10;
+        	if(flag == 1) {
+        		if(digits[i] + 1 > 10) {
+	        		digits[i] = (digits[i] + 1) % 10;
+	        		flag = 1;
+	        		if(i == 0) {
+	        			digits.insert(digits.begin(), 1);
+	        		}
+        		} else {
+        			digits[i] += 1;
+        			flag = 0;
+        		}
+        	}
         }
 
-        for(int i = digit2.size() - 1; i >= 0 ; i--) {
-        	ans.push_back(digit2[i]);
-        }
-
-        return ans;
+        return digits;
     }
 };
