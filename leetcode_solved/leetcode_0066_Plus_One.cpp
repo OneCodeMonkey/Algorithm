0d201a338aa8441e1ece0132a69dc53c6c9076e7
OneCodeMonkey <1460018362@qq.com>
2019-09-19 12:20:18 +0800

    [AC]leetcode_0066_Plus_One

--- leetcode_solved/leetcode_0066_Plus_One.cpp
+++ leetcode_solved/leetcode_0066_Plus_One.cpp
@@ -1,3 +1,10 @@
+/**
+ * AC:
+ * Runtime: 4 ms, faster than 65.48% of C++ online submissions for Plus One.
+ * Memory Usage: 8.3 MB, less than 100.00% of C++ online submissions for Plus One.
+ * 思路：写一个按位递进的判断，用一个变量记录上一个位置有无进位就行了。
+ *
+ */
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
