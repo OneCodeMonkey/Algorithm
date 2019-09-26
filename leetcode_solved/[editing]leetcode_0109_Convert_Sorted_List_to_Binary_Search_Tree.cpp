bbea12d8e5d1bec166fca9031f071586d12a97e1
OneCodeMonkey <1460018362@qq.com>
2019-09-26 10:47:57 +0800

    daily

---
+++ leetcode_solved/[editing]leetcode_0109_Convert_Sorted_List_to_Binary_Search_Tree.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        
+    }
+};
