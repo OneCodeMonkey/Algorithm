7075e1c4006b969cbdb773c4b60149c89c898834
OneCodeMonkey <1460018362@qq.com>
2019-09-27 10:58:39 +0800

    [editing]leetcode_0116_Populating_Next_Right_Pointers_in_Each_Node

---
+++ leetcode_solved/[editing]leetcode_0116_Populating_Next_Right_Pointers_in_Each_Node.cpp
@@ -0,0 +1,25 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+public:
+    Node* connect(Node* root) {
+        
+    }
+};
