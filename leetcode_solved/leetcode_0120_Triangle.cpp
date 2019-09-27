f46e4090015dcee761ca0e5782a0eef49f6fa0fc
OneCodeMonkey <1460018362@qq.com>
2019-09-28 01:31:56 +0800

    leetcode_0120_Triangle

--- leetcode_solved/leetcode_0120_Triangle.cpp
+++ leetcode_solved/leetcode_0120_Triangle.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        
+        int n = triangle.size();
+        vector<int>record(triangle.back());
+        for(int i = n - 2; i >= 0; i--) {
+        	for(int j = 0; j <= i; j++) {
+        		record[j] = min(record[j], record[j + 1]) + triangle[i][j];
+        	}
+        }
+
+        return record[0];
     }
 };
