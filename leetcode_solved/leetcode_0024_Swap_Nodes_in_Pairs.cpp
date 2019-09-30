3ec56408ffe187870640ce524c6ae69a33040f15
OneCodeMonkey <1460018362@qq.com>
2019-09-30 16:23:49 +0800

    leetcode_0024_Swap_Nodes_in_Pairs

--- leetcode_solved/leetcode_0024_Swap_Nodes_in_Pairs.cpp
+++ leetcode_solved/leetcode_0024_Swap_Nodes_in_Pairs.cpp
@@ -13,6 +13,14 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        while(head)
+        ListNode **pp = &head, *a, *b;
+        while((a = *pp) && (b = a->next)) {
+        	a->next = b->next;
+        	b->next = a;
+        	*pp = b;
+        	pp = &(a->next);
+        }
+
+        return head;
     }
 };
