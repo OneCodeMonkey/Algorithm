a125d297ccb272d8e970cc5d29018012cbf6ee29
OneCodeMonkey <1460018362@qq.com>
2019-09-30 11:42:55 +0800

    leetcode_0136_Single_Number

--- leetcode_solved/[editing]leetcode_0136_Single_Number.cpp
+++ leetcode_solved/[editing]leetcode_0136_Single_Number.cpp
@@ -1,6 +1,27 @@
+/**
+ * AC：Runtime: 20 ms, faster than 34.84% of C++ online submissions for Single Number.
+ * Memory Usage: 9.9 MB, less than 65.43% of C++ online submissions for Single Number.
+ *
+ */
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        
+        sort(nums.begin(), nums.end());
+        int ret = nums[0];
+        int flag = false;	// ret 是否已配对
+        for(int i = 1; i < nums.size(); i++) {
+        	if(nums[i] == ret) {
+        		flag = true;
+        	} else {
+        		if(flag == true) {
+        			ret = nums[i];
+        			flag = false;
+        		} else {
+        			return ret;
+        		}
+        	}
+        }
+
+    	return ret;
     }
 };
