6a763d3c13413c4ae474c7d7f2942386630ba548
OneCodeMonkey <1460018362@qq.com>
2019-09-30 12:18:47 +0800

    qualified: leetcode_0136_Single_Number

--- leetcode_solved/leetcode_0136_Single_Number.cpp
+++ leetcode_solved/leetcode_0136_Single_Number.cpp
@@ -1,27 +1,17 @@
 /**
- * AC：Runtime: 20 ms, faster than 34.84% of C++ online submissions for Single Number.
- * Memory Usage: 9.9 MB, less than 65.43% of C++ online submissions for Single Number.
- *
+ * Runtime: 20 ms, faster than 34.84% of C++ online submissions for Single Number.
+ * Memory Usage: 9.6 MB, less than 100.00% of C++ online submissions for Single Number. *
+ * 
+ * 思路：用异或运算 ^= , 0 ^ A = A, A ^ A = 0, 有配对就会抵消一次运算造成的影响。
+ * 传统的思路，既不能排序，也不能用至少 O(n) 的额外空间，是办不到的。
  */
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int ret = nums[0];
-        int flag = false;	// ret 是否已配对
-        for(int i = 1; i < nums.size(); i++) {
-        	if(nums[i] == ret) {
-        		flag = true;
-        	} else {
-        		if(flag == true) {
-        			ret = nums[i];
-        			flag = false;
-        		} else {
-        			return ret;
-        		}
-        	}
+        int ret = 0;
+        for(int i = 0; i < nums.size(); i++) {
+        	ret ^= nums[i];
         }
-
-    	return ret;
+        return ret;
     }
 };
