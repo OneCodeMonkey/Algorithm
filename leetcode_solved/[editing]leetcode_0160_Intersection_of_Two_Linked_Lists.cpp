2308c2dabf3c3046c5311b844fb7a19defaeb09c
OneCodeMonkey <1460018362@qq.com>
2019-10-04 23:07:40 +0800

    [editing]leetcode_0160_Intersection_of_Two_Linked_Lists

---
+++ leetcode_solved/[editing]leetcode_0160_Intersection_of_Two_Linked_Lists.cpp
@@ -0,0 +1,18 @@
+/**
+ * 思路：经典的双链表求交点题。用快慢指针法
+ *
+ */ 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+    }
+};
