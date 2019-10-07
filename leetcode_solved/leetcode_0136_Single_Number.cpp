2e9f5826e1e7a45c5713a0cad705efd323398fee
OneCodeMonkey <1460018362@qq.com>
2019-10-07 17:37:17 +0800

    update

--- leetcode_solved/leetcode_0136_Single_Number.cpp
+++ leetcode_solved/leetcode_0136_Single_Number.cpp
@@ -1,6 +1,6 @@
 /**
- * Runtime: 20 ms, faster than 34.84% of C++ online submissions for Single Number.
- * Memory Usage: 9.6 MB, less than 100.00% of C++ online submissions for Single Number. *
+ * Runtime: 16 ms, faster than 67.11% of C++ online submissions for Single Number.
+ * Memory Usage: 9.7 MB, less than 93.83% of C++ online submissions for Single Number.
  * 
  * 思路：用异或运算 ^= , 0 ^ A = A, A ^ A = 0, 有配对就会抵消一次运算造成的影响。
  * 传统的思路，既不能排序，也不能用至少 O(n) 的额外空间，是办不到的。
