6643009744c60660f988f272bf946beac9f81489
OneCodeMonkey <1460018362@qq.com>
2019-10-07 12:29:49 +0800

    leetcode_0179_Largest_Number

---
+++ leetcode_solved/leetcode_0179_Largest_Number.cpp
@@ -0,0 +1,19 @@
+/**
+ * 思路：按字典序来排序拼接出来的字符串，得到的最大的就是可以拼出的最大数字
+ *
+ */
+class Solution {
+public:
+    string largestNumber(vector<int> &num) {
+        vector<string> arr;
+        for(auto i:num)
+            arr.push_back(to_string(i));
+        sort(begin(arr), end(arr), [](string &s1, string &s2){ return s1+s2>s2+s1; });
+        string res;
+        for(auto s:arr)
+            res+=s;
+        while(res[0]=='0' && res.length()>1)
+            res.erase(0,1);
+        return  res;
+    }
+};
