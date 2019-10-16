e361acf4c430cfe32a68c660a3c0cfade34181bb
OneCodeMonkey <1460018362@qq.com>
2019-10-16 11:39:59 +0800

    leetcode_0226_Invert_Binary_Tree

--- leetcode_solved/[editing]leetcode_0226_Invert_Binary_Tree.cpp
+++ leetcode_solved/[editing]leetcode_0226_Invert_Binary_Tree.cpp
@@ -1,1 +1,27 @@
-[editing]leetcode_0226_Invert_Binary_Tree.cpp
+/**
+ * AC:
+ * Runtime: 0 ms, faster than 100.00% of C++ online submissions for Invert Binary Tree.
+ * Memory Usage: 9.2 MB, less than 81.82% of C++ online submissions for Invert Binary Tree.
+ *
+ * 思路：递归,对每一级进行反转
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+     	if(root) {
+     		invertTree(root->left);
+     		invertTree(root->right);
+     		std::swap(root->left, root->right);
+     	}
+     	return root;
+    }
+};
