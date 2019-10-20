6d1f8baaf628e64cf4d6de3ed80847c3ecc49919
OneCodeMonkey <1460018362@qq.com>
2019-10-20 14:42:28 +0800

    leetcode_0027_Remove_Element

--- leetcode_solved/[editing]leetcode_0027_Remove_Element.cpp
+++ leetcode_solved/[editing]leetcode_0027_Remove_Element.cpp
@@ -1,6 +1,26 @@
+/**
+ * AC：
+ * Runtime: 0 ms, faster than 100.00% of C++ online submissions for Remove Element.
+ * Memory Usage: 8.7 MB, less than 38.23% of C++ online submissions for Remove Element.
+ *
+ * 思路：原地算法，就是不使用额外空间完成预期功能。
+ * 这里简单用原数组位置进行替换赋值即可。最后它只会去 return 的值
+ * 的 N 位取结果数组。这是这道题隐藏的返回特性。
+ */
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        
+    	int size = nums.size();
+        if(size == 0)
+        	return 0;
+
+        int count = 0;
+        for(int i = 0; i < size; i++) {
+        	if(nums[i] != val) {
+        		nums[count++] = nums[i];
+        	}
+        }
+
+        return count;
     }
 };
