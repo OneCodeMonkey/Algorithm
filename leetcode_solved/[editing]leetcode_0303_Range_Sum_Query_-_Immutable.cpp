723942eaf93ec4be92681915ac608d5bd6d3292c
OneCodeMonkey <1460018362@qq.com>
2019-10-22 14:53:00 +0800

    leetcode_0303_Range_Sum_Query_-_Immutable

--- leetcode_solved/[editing]leetcode_0303_Range_Sum_Query_-_Immutable.cpp
+++ leetcode_solved/[editing]leetcode_0303_Range_Sum_Query_-_Immutable.cpp
@@ -1,12 +1,20 @@
+/**
+ * AC:
+ * Runtime: 28 ms, faster than 93.42% of C++ online submissions for Range Sum Query - Immutable.
+ * Memory Usage: 17.1 MB, less than 96.55% of C++ online submissions for Range Sum Query - Immutable.
+ */
 class NumArray {
 public:
-    NumArray(vector<int>& nums) {
-        
+    NumArray(vector<int>& nums):record(nums.size() + 1, 0) {
+        partial_sum(nums.begin(), nums.end(), record.begin() + 1);
     }
     
     int sumRange(int i, int j) {
-        
+  		return record[j + 1] - record[i];      
     }
+
+private:
+	vector<int> record;    
 };
 
 /**
