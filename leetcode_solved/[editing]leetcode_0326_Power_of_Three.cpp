38930ece15b900de3c7841860b90efa89eaa60b1
OneCodeMonkey <1460018362@qq.com>
2019-10-25 12:12:41 +0800

    leetcode_0326_Power_of_Three

--- leetcode_solved/[editing]leetcode_0326_Power_of_Three.cpp
+++ leetcode_solved/[editing]leetcode_0326_Power_of_Three.cpp
@@ -1,6 +1,17 @@
+/**
+ * AC：
+ * Runtime: 16 ms, faster than 67.62% of C++ online submissions for Power of Three.
+ * Memory Usage: 8.2 MB, less than 66.67% of C++ online submissions for Power of Three.
+ * 思路: 转换为求 log3(n) 是否是整数。
+ *
+ */
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        
+        double power = log10(n) / log10(3);
+        if(fmod(power, 1) == 0)		// 指数为整数
+        	return true;
+
+        return false;
     }
 };
