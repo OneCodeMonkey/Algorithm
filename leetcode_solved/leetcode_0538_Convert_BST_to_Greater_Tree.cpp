23e2cf8e9889c48ed2197ebfea4e42c45210cb2e
OneCodeMonkey <1460018362@qq.com>
2019-11-28 15:47:49 +0800

    leetcode_0538_Convert_BST_to_Greater_Tree

--- leetcode_solved/leetcode_0538_Convert_BST_to_Greater_Tree.cpp
+++ leetcode_solved/leetcode_0538_Convert_BST_to_Greater_Tree.cpp
@@ -8,8 +8,20 @@
  * };
  */
 class Solution {
+private:
+	int sum = 0;
 public:
+	void travel(TreeNode* root) {
+		if(!root)
+			return;
+		if(root->right)
+			travel(root->right);
+		root->val = (sum += root->val);
+		if(root->left)
+			travel(root->left);
+	}
     TreeNode* convertBST(TreeNode* root) {
-        
+        travel(root);
+        return root;
     }
 };
