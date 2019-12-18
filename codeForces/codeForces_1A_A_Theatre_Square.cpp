4d89639f03202a6692657880b782375e28af5406
OneCodeMonkey <1460018362@qq.com>
2019-12-18 15:21:51 +0800

    optimize

--- codeForces/codeForces_1A_A_Theatre_Square.cpp
+++ codeForces/codeForces_1A_A_Theatre_Square.cpp
@@ -2,34 +2,22 @@
  * 思路：矩形用小正方形填充，只需行和列取整就行了。
  * 注意陷阱，不要用 int32，否则结果打印不了最大 10^18 这样的大数
  * AC：
- * Time:31 ms	Memory:0 KB
+ * Time:30 ms	Memory:0 KB
  *
  */
 #include<stdio.h>
 #include<map>
 #include<utility>
+#include<math.h>
 
 using namespace std;
 
 int main()
 {
 	int n, m, a;
-	long long row = 0, col = 0, ans = 0;
+	long long ans = 0;
 	scanf("%d%d%d", &n, &m, &a);
-	if (n % a == 0) {
-		col = n / a;
-	}
-	else {
-		col = n / a + 1;
-	}
-	if (m % a == 0) {
-		row = m / a;
-	}
-	else {
-		row = m / a + 1;
-	}
-
-	ans = row * col;
+	ans = ceil((double)n / a) * ceil((double)m / a);	// floor() 和 ceil() 的入参都是 double型
 	printf("%lld", ans);
 	return 0;
 }
