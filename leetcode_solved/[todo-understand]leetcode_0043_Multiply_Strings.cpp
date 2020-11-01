5bf6f4fa07934f6951b4501ad4c28c6d4a455ed7
OneCodeMonkey <1460018362@qq.com>
2020-11-02 00:05:57 +0800

    update

---
+++ leetcode_solved/[todo-understand]leetcode_0043_Multiply_Strings.cpp
@@ -0,0 +1,24 @@
+// 大数乘法
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        string sum(num1.size() + num2.size(), '0');
+
+        for (int i = num1.size() - 1; i >= 0; i--) {
+        	int carry = 0;
+        	for (int j = num2.size() - 1; j >= 0; j--) {
+        		int temp = (sum[i + j + 1] - '0') + (num1[i] - '0') * (num2[j] - '0') + carry;
+        		sum[i + j + 1] = temp % 10 + '0';
+        		carry = temp / 10;
+        	}
+        	sum[i] += carry;
+        }
+
+        size_t startpos = sum.find_first_not_of("0");
+        if (string::npos != startpos) {
+        	return sum.substr(startpos);
+        }
+
+        return "0";
+    }
+};
