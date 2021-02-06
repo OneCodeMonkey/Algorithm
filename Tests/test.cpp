84e6e9dc5f5da6e12504e060536cd9c01484c19f
OneCodeMonkey <1460018362@qq.com>
2021-02-06 18:02:57 +0800

    feat

---
+++ Tests/test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by admin on 2021/2/6.
+//
+#include<stdio.h>
+
+int main() {
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+
+    printf("hello world!\n");
+    return 0;
+}
