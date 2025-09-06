121d252883f2ff1946a52c894efeed66c3c9f0c2
OneCodeMonkey <1460018362@qq.com>
2025-09-06 13:00:36 +0800

    modify

--- leetcode_solved/leetcode_0037_Sudoku_Solver.cpp
+++ leetcode_solved/leetcode_0037_Sudoku_Solver.cpp
@@ -62,3 +62,45 @@ public:
        	DFS(0, board, isSuccess);
     }
 };
+
+
+// Solution 2:
+
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+
+private:
+    bool solve(vector<vector<char>>& board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) return true;
+                            board[row][col] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool isValid(vector<vector<char>>& board, int row, int col, char num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num) return false;
+        }
+        int startRow = (row / 3) * 3, startCol = (col / 3) * 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == num) return false;
+            }
+        }
+        return true;
+    }
+};
